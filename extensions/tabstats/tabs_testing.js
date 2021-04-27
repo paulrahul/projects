b89ea32dcf0a89a773e91ded355ce4e1de9d9c73
paulrahul <paul.rahul@gmail.com>
2021-04-27 23:52:33 +0200

    Added await and Promise instrumentation. Still W-I-P.

--- extensions/tabstats/tabs_testing.js
+++ extensions/tabstats/tabs_testing.js
@@ -1,53 +1,63 @@
 url_id_map = {}
 
-async function sleep(seconds) {
-    await new Promise(
-        r => setTimeout(r,
-                        seconds * 1000));
-}
-
-function runTest() {
-    createTestTab("google.com")
-    createTestTab("reddit.com", 1)
-    createTestTab("news.ycombinator.com", 2)
-
-    visitTestTab("reddit.com", 3)
-    deleteTestTab("reddit.com", 4)
-}
-
-async function createTestTab(url, delay_mins=null) {
-    if (delay_mins) {
-        sleep(delay_mins * 60)
-    }
-
+async function createTestTab(url) {
     createProperties = {
         url: url,
         active: true
     };
 
-    chrome.tabs.create(createProperties, function(tab) {
-        url_id_map[url] = tab.id
-    });
+    // chrome.tabs.create(createProperties, function(tab) {
+    //     url_id_map[url] = tab.id
+    // });
 }
 
-async function visitTestTab(url, delay_mins=null) {
-    if (delay_mins) {
-        sleep(delay_mins * 60)
-    }
-
+async function visitTestTab(url) {
     highlightInfo = {
         tabs: [url_id_map[url]]
     };
 
-    chrome.tabs.highlight(createProperties, function(window) {
-    });
+    // chrome.tabs.highlight(createProperties, function(window) {
+    // });
 }
 
-function deleteTestTab(url, delay_mins=null) {
+function deleteTestTab(url) {
+    // chrome.tabs.remove(url_id_map[url], function() {
+    // });
+}
+
+function sleep(seconds) {
+    return new Promise(
+        r => setTimeout(r,
+                        seconds * 1000));
+}
+
+async function performAfterDelay(cb, delay_mins=null) {
     if (delay_mins) {
-        sleep(delay_mins * 60)
+        // console.log("Before sleep of " + delay_mins)
+        await sleep(delay_mins * 60)
+        // console.log("After sleep")
     }
-
-    chrome.tabs.remove(url_id_map[url], function() {
-    });
+    await cb()
 }
+
+async function runTest() {
+    await performAfterDelay(function() {
+        createTestTab("google.com")
+    })
+    await performAfterDelay(function() {
+        createTestTab("reddit.com")
+    }, 1)
+    await performAfterDelay(function() {
+        createTestTab("news.ycombinator.com")
+    }, 2)
+
+    await performAfterDelay(function() {
+        visitTestTab("reddit.com")
+    }, 3)
+
+    await performAfterDelay(function() {
+        deleteTestTab("reddit.com")
+    }, 4)
+}
+
+runTest()
